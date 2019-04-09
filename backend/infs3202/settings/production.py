3c47f355a056d9cff87dbebb43d2ea64828e8d75
Eivind Reime <22083159+eivrei@users.noreply.github.com>
2019-04-10 00:22:50 +1000

    Create  backend settings for production

---
+++ backend/infs3202/settings/production.py
@@ -0,0 +1,5 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['151.101.1.195', '151.101.65.195', 'thebigevent.xyz', 'www.thebigevent.xyz']
--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infs3202.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infs3202.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
