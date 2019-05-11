ce4d533df4297a84e86beacc6c7660572905543a
Eivind Reime <22083159+eivrei@users.noreply.github.com>
2019-05-11 16:06:27 +1000

    Migrate to use environment file

--- backend/infs3202/settings/production.py
+++ backend/infs3202/settings/production.py
@@ -1,9 +1,16 @@
 from .base import *
+import environ
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(os.path.dirname(BASE_DIR), '.env'))
 
 DEBUG = False
 
-SECRET_KEY = ''  # Add secret key here
+SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = ['localhost', '13.58.204.87', 'api.thebigevent.xyz']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
-FRONTEND_URL = 'https://thebigevent.xyz'
+FRONTEND_URL = env('FRONTEND_URL')
+
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
