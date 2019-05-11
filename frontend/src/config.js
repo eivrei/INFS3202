97e3b09c52f24a64dba715ad1f064c90a406dcea
Eivind Reime <22083159+eivrei@users.noreply.github.com>
2019-05-11 21:34:51 +1000

    Add reCAPTCHA v2 to sign up form

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,6 @@
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+export const RECAPTCHA_KEY =
+  process.env.RECAPTCHA_KEY || '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
 export const BASE_HEADERS = {
   'Content-Type': 'application/json'
 };
