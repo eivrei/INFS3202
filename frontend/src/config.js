68587d1bed43659cce588e08b67e14f1614c0e60
Eivind Reime <22083159+eivrei@users.noreply.github.com>
2019-05-15 12:42:46 +1000

    Fix environment variable issue

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 export const RECAPTCHA_KEY =
-  process.env.RECAPTCHA_KEY || '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
+  process.env.REACT_APP_RECAPTCHA_KEY || '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
 export const BASE_HEADERS = {
   'Content-Type': 'application/json'
 };
